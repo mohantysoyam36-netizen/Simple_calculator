974653e5447f6fc3d4e8cf21660a0530e51a24ea
mohantysoyam36-netizen <mohantysoyam36@gmail.com>
2025-12-16 18:33:27 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,12 @@
+def calc(a,b,op):
+    if op=='+':
+        return a+b
+    elif op=='-':
+        return a-b
+    elif op=='*':
+        return a*b
+    elif op=='/':
+        return a/b
+    else:
+       'Invalid input' 
+print (calc(10,5,'/'))
